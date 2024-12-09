e46604602ce5eb317d2d2293e63145ecf9305627
Devinda Wijesingha <devindawijesingha021@gmail.com>
2024-12-09 06:07:54 +0530

    feat: implement configuration form and display components

---
+++ FrontEnd/src/app/components/configuration-form/configuration-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './configuration-form.component.html',
+  styleUrl: './configuration-form.component.scss'
+})
+export class ConfigurationFormComponent {
+  totalTickets: number | null = null;
+  ticketReleaseRate: number | null = null;
+  customerRetrievalRate: number | null = null;
+  maxTicketCapacity: number | null = null;
+
+  onSubmit() {
+    console.log('Form Submitted:',{
+      totalTickets: this.totalTickets,
+      ticketReleaseRate: this.ticketReleaseRate,
+      customerRetrievalRate: this.customerRetrievalRate,
+      maxTicketCapacity: this.maxTicketCapacity
+    });
+  }
+}
